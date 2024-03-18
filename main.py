d5e8eef35502febea9e2428e0f207921d4c80e03
root <root@LAPTOP-UT940GA6>
2024-03-18 16:16:37 +0100

    added function to get inputs from the user

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    print("Welcome to my calculator!")
+    
+    get_input()
+
+
+def get_input():
+    a = input("Provide a: ")
+    b = input("Provde b: ")
+    return a, b 
+
+
+
+main()
