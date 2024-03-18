ec13c01fbc3abeb0cd8dac515200594722136281
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-03-18 16:32:43 +0100

    Updated the input function to accept only numbers. Also aded a new function to choose operation

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 def main():
     print("Welcome to my calculator!")
-    
     get_input()
-
+    choose_op()
+    
 
 def get_input():
-    a = input("Provide a: ")
-    b = input("Provde b: ")
-    return a, b 
+    while True:
+        a = input("Provide a: ")
+        if not a.isdigit():
+            print("You need to provide a number.")
+        else:
+            break
+    while True:
+        b = input("Provde b: ")
+        if not b.isdigit():
+            print("You need to provide a number.")
+        else:
+            break
+    return a, b
+
+def choose_op():
+    print("Choose operation:")
+    print("1 - addition\n2 - subtraction\n3 - multiplication\n4 - division")
+    operation = input()
+    return operation
 
 
 
